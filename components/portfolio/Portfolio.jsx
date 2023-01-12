e40a489da5d281be77356a579dd1b19adc89f3df
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-01-12 16:38:42 -0600

    Se agregaron imagenes para las secciones de proyectos y se orrigieron los estilos css, los cuales aún se deben continuar mejorando

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -6,8 +6,21 @@ function Portfolio(){
         <section className={styles.sectionPortfolio} id="PortfolioArea">
         <h2 className={styles.sectionPortfolio__h2+" tR_Xlarge"}>Mis Proyectos :</h2>
         <dir className={styles.sectionPortfolio__divProjects}>
-            <ProjectCard linkView ="https://super-games.vercel.app/login" linkGithub ="https://github.com/JosueElutiche24/SuperGames" Title="Super Games" Image="/gamerPanda.jpeg" Collection={["/gamerPanda.jpeg","guitarPanda.jpeg","/moonPanda.jpeg"]} Description="Un sitio en el que te puedes registrar y jugar a memoria, busca minas y snake. Almacenamos la puntuación que obtengas en tus mejores partidas para que compitas con los mejores usuarios." ConocimientosEmpleados="Sistema de registro de usuarios, rutas protegidas y generación de tokens almacenados en cookies utilizando JsonWebToken, control de diversos useEffect y useStates para la creación de los controles de los juegos, se utilizó arrays bidimensionales para buscaminas y snake. El backend fue construido en el mismo proyecto sobre Next js con una estructura de  Api rest, con una base de datos Mysql alojada en internet con el servicio de  PlanetScale." Tecnologias={["React","Next Js","Tailwind","JWT","NySql","Vercel","PlanetScale",]}/>
-            <ProjectCard linkView ="https://www.jguzman.tk/" linkGithub ="https://github.com/JosueElutiche24/PortafolioProfesional" Title="Mi sitio Web" Image="/moonPanda.jpeg" Collection={["No disponible"]} Description="Este sitio web fue creado para mostrar mis proyectos y las tecnologías con las que trabajo, también con la intención de poder ser contactado por personas interesadas en lo que hago." ConocimientosEmpleados="En el desarrollo de este proyecto se utilizó: Principalmente un alto nivel de Css. Buen manejo de componentes y Hoocks de react. Implementación del formulario de contacto con API de Gmail. Despliegue del sitio web en hosting y adquisición de un dominio propio." Tecnologias={["React", "NextJs", "Vercel","emailJS", "Tsparticles"]}/>
+            <ProjectCard linkView ="https://super-games.vercel.app/login" 
+            linkGithub ="https://github.com/JosueElutiche24/SuperGames" 
+            Title="Super Games" Image="/projectImages/SuperGames.jpeg" 
+            Collection={["/Project2/img1.jpeg","/Project2/img2.jpeg","/Project2/img3.jpeg","/Project2/img4.jpeg","/Project2/img5.jpeg"]} 
+            Description="Un sitio en el que te puedes registrar y jugar a memoria, busca minas y snake. Almacenamos la puntuación que obtengas en tus mejores partidas para que compitas con los mejores usuarios." 
+            ConocimientosEmpleados="Sistema de registro de usuarios, rutas protegidas y generación de tokens almacenados en cookies utilizando JsonWebToken, control de diversos useEffect y useStates para la creación de los controles de los juegos, se utilizó arrays bidimensionales para buscaminas y snake. El backend fue construido en el mismo proyecto sobre Next js con una estructura de  Api rest, con una base de datos Mysql alojada en internet con el servicio de  PlanetScale." 
+            Tecnologias={["React","Next Js","Tailwind","JWT","NySql","Vercel","PlanetScale",]}/>
+
+            <ProjectCard linkView ="https://www.jguzman.tk/" 
+            linkGithub ="https://github.com/JosueElutiche24/PortafolioProfesional" 
+            Title="Mi sitio Web" Image="/projectImages/Portfolio.jpeg" 
+            Collection={["/Project1/img1.jpeg","/Project1/img2.jpeg","/Project1/img3.jpeg","/Project1/img4.jpeg","/Project1/img5.jpeg"]} 
+            Description="Este sitio web fue creado para mostrar mis proyectos y las tecnologías con las que trabajo, también con la intención de poder ser contactado por personas interesadas en lo que hago." 
+            ConocimientosEmpleados="En el desarrollo de este proyecto se utilizó: Principalmente un alto nivel de Css. Buen manejo de componentes y Hoocks de react. Implementación del formulario de contacto con API de Gmail. Despliegue del sitio web en hosting y adquisición de un dominio propio." 
+            Tecnologias={["React", "NextJs", "Vercel","emailJS", "Tsparticles","SwiperJs"]}/>
         </dir>
         </section>
     )
--- components/portfolio/Projects.jsx
+++ components/portfolio/Projects.jsx
@@ -64,28 +64,29 @@ return (
                     {/* Sector del swiper */}
 
                 </div>
-                <div className={stylesCards.modal_DivConocimienos}>
-                    <p className={stylesCards.ConoTitle+" tR_medium"}>Conocimiento empleado</p>
-                    <p className={stylesCards.ConoParragraph}>{ConocimientosEmpleados}</p>
-                    <button className={stylesCards.btnClosedProject+" tR_small"} onClick={flipCard}>volver</button>
+                <div className={stylesCards.modal_subContent}>
+                    <div className={stylesCards.modal_DivTecnos}>
+                        <p className={stylesCards.tecnosTitle+" tR_medium"}>Tecnologías utilizadas</p>
+                        <div className={stylesCards.tecnosElements}>
+                            {Tecnologias.map((item, index)=>{
+                                return <p  className={stylesCards.tecnosElement+" tR_small"} key={index}>{item}</p>
+                            })}
+                        </div>
+                    </div>
+                    <div className={stylesCards.modal_DivLinks}>
+                            <p className={stylesCards.linksTitle+" tR_medium"}>Enlaces</p>
+                    {linkView !== "ModoDes" && <p className={stylesCards.linksEnlaceSitio+" tR_small"}>Visitar sitio : <a href={linkView}>{linkView}</a></p>}
+                            <p className={stylesCards.linksEnlaceGit+" tR_small"}>Repositorio en github : <a href={linkGithub}>{linkGithub}</a></p>
+                    </div>
                 </div>
             </div>
             <div className={stylesCards.modal_content_2}>
-                <div className={stylesCards.modal_DivTecnos}>
-                    <p className={stylesCards.tecnosTitle+" tR_medium"}>Tecnologías utilizadas</p>
-                    <div className={stylesCards.tecnosElements}>
-                        {Tecnologias.map((item, index)=>{
-                            return <p  className={stylesCards.tecnosElement+" tR_small"} key={index}>{item}</p>
-                        })}
-                    </div>
-                </div>
-                <div className={stylesCards.modal_DivLinks}>
-                        <p className={stylesCards.linksTitle+" tR_medium"}>Enlaces</p>
-                {linkView !== "ModoDes" && <p className={stylesCards.linksEnlaceSitio+" tR_small"}>Visitar sitio : <a href={linkView}>{linkView}</a></p>}
-                        <p className={stylesCards.linksEnlaceGit+" tR_small"}>Repositorio en github : <a href={linkGithub}>{linkGithub}</a></p>
+                <div className={stylesCards.modal_DivConocimienos}>
+                        <p className={stylesCards.ConoTitle+" tR_medium"}>Conocimiento empleado</p>
+                        <p className={stylesCards.ConoParragraph}>{ConocimientosEmpleados}</p>
+                        <button className={stylesCards.btnClosedProject+" tR_small"} onClick={flipCard}>volver</button>
                 </div>
             </div>
-            
         </div>
     </section>}
     </>
